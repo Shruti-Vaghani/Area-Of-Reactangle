4132f3d94e80a2d3a1c19d9e0541bb475eb3b4e7
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2021-09-15 14:18:51 +0530

    [Ajesh/Shruthi] REFACTOR. variable name in Rectangle class

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,10 +1,10 @@
 public class Rectangle {
-    private double width;
-    private double height;
+    private final double width;
+    private final double height;
 
-    public Rectangle(double width, double length) {
+    public Rectangle(double width, double height) {
         this.width = width;
-        this.height = length;
+        this.height = height;
     }
 
     public double area(){
